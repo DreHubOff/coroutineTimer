9b487da52eaa5d28b709a1187cc400bf3be4a3d1
AndreBitSafe <andrey.com.dr2000@gmail.com>
2020-06-03 22:00:16 +0300

    init

---
+++ app/src/main/java/com/studying/simpletimer/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.studying.oroutinetimer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.studying.simpletimer.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        start.setOnClickListener {
+            val text = edit_text.text.toString()
+            if (text.isNotEmpty()) GlobalScope.launch { count(text.toInt()) }
+        }
+    }
+
+    private suspend fun count(secNum: Int) {
+        changeStatus(false)
+        var i = secNum
+        do {
+            withContext(Dispatchers.Main) { text_view.text = i.toString() }
+            i--
+            delay(1000)
+        } while (i > 0)
+        changeStatus(true)
+    }
+
+    private suspend fun changeStatus(flag: Boolean) {
+        withContext(Dispatchers.Main) {
+            start.isEnabled = flag
+            edit_text.isEnabled = flag
+        }
+    }
+}
